7c889c0b50e29350dce915c7dbe3ac14e12062dc
Charles <charles.jacquin@autistici.org>
2020-01-25 18:15:37 +0100

    chore: remove useless plugin and md related stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,3 @@
-'use strict'
-
 module.exports = {
   siteMetadata: {
     title: 'gatsby-starter-typescript-plus',
@@ -14,38 +12,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'content',
-        path: `${__dirname}/src/content`
-      }
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: {
-              wrapperStyle: 'margin-bottom: 1rem'
-            }
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 1140,
-              quality: 90,
-              linkImagesToOriginal: false
-            }
-          }
-        ]
-      }
-    },
-    'gatsby-transformer-json',
-    {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
         siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com'
@@ -54,7 +20,6 @@ module.exports = {
     'gatsby-plugin-emotion',
     'gatsby-plugin-typescript',
     'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet'
   ]
 }
--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -13,9 +13,6 @@ const PageTwo = () => (
         <p>Welcome to page 2</p>
         <ul>
           <li>
-            <Link to="/a-markdown-page/">Show me some Markdown!</Link>
-          </li>
-          <li>
             <Link to="/">Take me back home.</Link>
           </li>
         </ul>
