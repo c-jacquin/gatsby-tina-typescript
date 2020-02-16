327dfe8662bdd3550153202d2dfbddc321742ba3
Charles <charles.jacquin@autistici.org>
2020-02-16 13:42:57 +0100

    chore: add csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     'gatsby-plugin-typescript',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-csp',
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
